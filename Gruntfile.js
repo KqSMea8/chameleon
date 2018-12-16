f632e47db142db25875a1c66285f538eb82cc45d
jiaxuan <jiaxuan@didichuxing.com>
2018-12-17 01:25:32 +0800

    added

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,3 @@
-/*!
- * Bootstrap's Gruntfile
- * http://getbootstrap.com
- * Copyright 2013-2016 Twitter, Inc.
- * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
- */
 
 module.exports = function (grunt) {
   'use strict';
